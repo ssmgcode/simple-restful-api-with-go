363dd946decd7b5803f1d43ea42794baa86165a1
ssmg <ssmg.sg@gmail.com>
2021-09-15 08:58:14 -0600

    Get all articles
    Create Article and Articles type

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(rw http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Description: "Test Description", Content: "Test Content"},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(articles)
+}
 
 func homePage(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "Homepage Endpoint Hit")
@@ -12,6 +30,7 @@ func homePage(rw http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
